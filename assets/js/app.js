2d8eeddd0e6a5f7a7ac386358b06edc3f2f451a8
alexSam <alexsamalot19@gmail.com>
2019-09-17 13:03:21 -0400

    clean code with a .gitignore and correct order of elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,125 +1,120 @@
-      // Initial array of movies
-      var movies = ["Celtics", "Sixers", "Knicks", "Nets"];//needs to be longer and actually add
+// Initial array of NBA teams: it is recommended to use the city as well
+var topics = ["Celtics", "Sixers", "Knicks", "Nets", "Utah Jazz", "Miami Heat"];
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
-      function displayMovieInfo() {
+// displayTeamInfo function re-renders the HTML to display the appropriate content
+function displayTeamInfo() {
 
-        var team = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-          team + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+  var team = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    team + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
-        // Creating an AJAX call for the specific movie button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function (response) {
-          console.log(response)
-          for (var i = 0; i < 10; i++) {
-            // response.data.length
-            // Creating a div to hold the movie
-            var teamDiv = $("<div class='team'>");
+  // Creating an AJAX call for the specific NBA Team button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+    for (var i = 0; i < 10; i++) {
+      // response.data.length could also be used
 
-            // Storing the rating data
-            var rating = response.data[i].rating;
+      // Creating a div to hold the team
+      var teamDiv = $("<div class='team'>");
 
-            // Creating an element to have the rating displayed
+      // Storing the rating data
+      var rating = response.data[i].rating;
 
-            var pOne = $("<p>").text("Rating: " + rating);
-
-
-            console.log(pOne)
-            // Displaying the rating
-            teamDiv.append(pOne);
-
-            var stillURL = response.data[i].images.fixed_height_still.url;
-            var animateURL = response.data[i].images.fixed_height.url
-              ;//images.fixed_height.url
-            console.log(stillURL);
-            console.log(animateURL);
-            // Creating an element to hold the image
-            //readnotes: make argument seperate variable
-            var image = $("<img>").attr("src", stillURL);
+      // Creating an element to have the rating displayed
+      var pOne = $("<p>").text("Rating: " + rating);
 
 
+      console.log(pOne)
+      // Displaying the rating
+      teamDiv.append(pOne);
 
-            image.attr("class", "gif");
+      var stillURL = response.data[i].images.fixed_height_still.url;
+      var animateURL = response.data[i].images.fixed_height.url;
 
-            image.attr("data-still", stillURL);
-            image.attr("data-animate", animateURL);
-            image.attr("data-state", 'still');
+      // console.log(stillURL);
+      // console.log(animateURL);
 
-            // Appending the image
-            teamDiv.append(image);
+      // Creating an element to hold the image
+      var image = $("<img>").attr("src", stillURL);
+      //Set image attributes
+      image.attr("class", "gif");
+      image.attr("data-still", stillURL);
+      image.attr("data-animate", animateURL);
+      image.attr("data-state", 'still');
 
-            // Putting the entire movie above the previous movies
-            $("#team-view").prepend(teamDiv);
-          }
-        });
+      // Prepending the image
+      teamDiv.prepend(image);
 
-      }
+      // Putting the entire team above the previous teams
+      $("#team-view").prepend(teamDiv);
+    }
+  });
 
-      // Function for displaying movie data
-      function renderButtons() {
+}
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
+// Function for displaying team data
+function renderButtons() {
 
-        // Looping through the array of movies
-        for (var i = 0; i < movies.length; i++) {
+  // Deleting the teams prior to adding new teams
+  // (this is necessary otherwise you will have repeat buttons)
+  $("#buttons-view").empty();
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie-btn to our button
-          a.addClass("team-btn");
-          // Adding a data-attribute
-          a.attr("data-name", movies[i]);
-          // Providing the initial button text
-          a.text(movies[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
+  // Looping through the array of teams
+  for (var i = 0; i < topics.length; i++) {
 
-      // This function handles events where a movie button is clicked
-      $("#add-team").on("click", function (event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var movie = $("#movie-input").val().trim();
-// console.log(movie)
-        // Adding movie from the textbox to our array
-        movies.push(movie);
+    // Then dynamicaly generating buttons for each team in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class of team-btn to our button
+    a.addClass("team-btn");
+    // Adding a data-attribute
+    a.attr("data-name", topics[i]);
+    // Providing the initial button text
+    a.text(topics[i]);
+    // Adding the button to the buttons-view div
+    $("#buttons-view").append(a);
+  }
+}
 
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
+// This function handles events where a team button is clicked
+$("#add-team").on("click", function (event) {
+  event.preventDefault();
+  // This line grabs the input from the textbox
+  var teamVar = $("#team-input").val().trim();
 
-      // $("#team-movie").on("click", function(event) {
-      $(document.body).on('click', ".gif", function () {
-        event.preventDefault();
-        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-        var state = $(this).attr("data-state");
-        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-        // Then, set the image's data-state to animate
-        // Else set src to the data-still value
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
+  // Adding team from the textbox to our array
+  topics.push(teamVar);
+
+  // Calling renderButtons which handles the processing of our team array
+  renderButtons();
+});
+
+//Event handler/click listener for the gifs to animate or pause
+$(document.body).on('click', ".gif", function () {
+  event.preventDefault();
+  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  var state = $(this).attr("data-state");
+  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+  // Then, set the image's data-state to animate
+  // Else set src to the data-still value
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+  // console.log("ran off of a click")
+
+});
+
+// Adding a click event listener to all elements with a class of "team-btn"
+$(document).on("click", ".team-btn", displayTeamInfo);
 
 
-        console.log("ran off of a click")
-
-      });
-
-      // Adding a click event listener to all elements with a class of "movie-btn"
-      $(document).on("click", ".team-btn", displayMovieInfo);
-      // $(document).on("click", '.parentOfInfo > .gif', animateImage);
-
-      // Calling the renderButtons function to display the intial buttons
-      renderButtons();
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
 
